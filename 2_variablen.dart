beff4bc81c36aaecf23467b8435e46d64a26b152
matthiasmucha87 <147045642+matthiasmucha87@users.noreply.github.com>
2023-10-05 11:56:14 +0200

    Variablen

---
+++ 2_variablen.dart
@@ -0,0 +1,12 @@
+void main() {
+  int intAlter = 32; // Initialization
+  late int intAlter_2; // Declaration
+
+  intAlter_2 = 48; // Zuweisung
+
+  print(intAlter);
+  print(intAlter_2);
+
+  intAlter_2 = 50;
+  print(intAlter_2);
+}
