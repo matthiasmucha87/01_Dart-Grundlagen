6fc0e028582091ed0821cfb95cca8edc10596b52
matthiasmucha87 <147045642+matthiasmucha87@users.noreply.github.com>
2023-10-05 12:32:43 +0200

    Abfragen

---
+++ 07_switsch_case.dart
@@ -0,0 +1,14 @@
+void main() {
+  int alter = 17;
+
+  switch (alter) {
+    case 18:
+      print("ich bin 18");
+      break;
+    case 17:
+      print("ich bin 17");
+      break;
+    default:
+      print("keine Ahnung");
+  }
+}
